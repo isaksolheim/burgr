f77c1abab91fff066fa69bbee94f316e2658ebfe
wickdeeb <isak.m.h.s@gmail.com>
2019-05-01 20:19:30 +0200

    modal menu image added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,15 +1,36 @@
 import React from 'react';
 
-function Menu() {
-  return(
-    <div className="nes-container with-title" id="menu">
-      <p className="title">Menu</p>
-      <div className="menu-images">
-        <img src="burgr-2018-meny-burgers.jpg" alt="burgers" />
-        <img src="burgr-2018-meny-extras.jpg" alt="extras" />
+class Menu extends React.Component {
+  viewImage(imgId) {
+    var modal = document.getElementById('modal');
+    var img = document.getElementById(imgId);
+    var modalImg = document.getElementById('img01');
+    var captionText = document.getElementById('caption');
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+  }
+
+  closeImage() {
+    var modal = document.getElementById('modal');
+    modal.style.display = 'none';
+  }
+  render() {
+    return(
+      <div className="nes-container with-title" id="menu">
+        <p className="title">Menu</p>
+        <div className="img-container">
+          <img onClick={() => this.viewImage('burgers')} id="burgers" src="burgr-2018-meny-burgers.jpg" alt="burgr menu" />
+          <img onClick={() => this.viewImage('extras')} id="extras" src="burgr-2018-meny-extras.jpg" alt="extas menu" />
+        </div>
+        <div id="modal" className="modal">
+          <span onClick={this.closeImage} className="close">&times;</span>
+          <img className="modal-content" id="img01" alt="menu" />
+          <div id="caption"></div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Menu;
