63c6d3f26793de76badc1fa857881ab71ed5d5c4
wickdeeb <isak.m.h.s@gmail.com>
2019-05-02 18:31:08 +0200

    Updates

---
+++ precache-manifest.c31c1cbd77b89d6469ff5490036e2d1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d31ca4207c51c44f7867f8690e79b7",
+    "url": "/burgr/index.html"
+  },
+  {
+    "revision": "b4c8f5f542005d484837",
+    "url": "/burgr/static/css/main.c45e3be6.chunk.css"
+  },
+  {
+    "revision": "28e224039ebc7d08949a",
+    "url": "/burgr/static/js/2.faa5270a.chunk.js"
+  },
+  {
+    "revision": "b4c8f5f542005d484837",
+    "url": "/burgr/static/js/main.fbdd59d2.chunk.js"
+  },
+  {
+    "revision": "acde15a58c6fff6f49b9",
+    "url": "/burgr/static/js/runtime~main.c5f89fb0.js"
+  }
+]);
